a695494a4af115d17eeb8c2d5934d4ee4bae2fd3
Beamlak F <beamlakfekadu562@gmail.com>
2025-01-09 12:17:43 -0800

    added auth

---
+++ nest-app/src/auth/auth.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from '../user/user.service';
+import { LoginDto } from './dto/login.dto';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(loginDto: LoginDto): Promise<{ access_token: string }> {
+    const { email, password } = loginDto;
+    const user = await this.userService.findByEmail(email);
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+    const payload = { sub: user.id, email: user.email };
+    const token = this.jwtService.sign(payload);
+    return { access_token: token };
+  }
+}
--- nest-app/src/user/user.service.ts
+++ nest-app/src/user/user.service.ts
@@ -3,63 +3,69 @@ import { user } from 'src/entity/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dtos/createUser.dto';
-import * as bcrypt from 'bcryptjs';
+import * as bcrypt from 'bcrypt';
+
 @Injectable()
 export class UserService {
-    constructor(
-        @InjectRepository(user)
-        private readonly userRepository: Repository<user>,
-      ) {}
-    // create users
-    createUser(createUserDto: CreateUserDto): Promise<user> {
-        return this.userRepository
-          .findOne({
-            where: [{ username: createUserDto.username }, { email: createUserDto.email }],
-        })
-          .then(existingUser => {
-            if (existingUser) {
-              return Promise.reject(new Error('Username or Email already taken'));
-            }
-    
-            return bcrypt
-              .hash(createUserDto.password, 10)
-              .then(hashedPassword => {
-                const user = this.userRepository.create({
-                  username: createUserDto.username,
-                  email: createUserDto.email,
-                  password: hashedPassword,
-                });
-    
-                return this.userRepository.save(user);
-              });
-          });
-    }
-    // userExists
-    userExists(username: string): Promise<boolean> {
-        return this.userRepository
-          .findOne({ where: { username } })
-          .then((user) => !!user);
-    } 
-    // Fetch User Profile (Get user details)
-    fetchUserProfile(id: number) {
-        return this.userRepository.findOneBy({ id });
-      }
-    // validate users
-    validateUser(username: string, password: string): Promise<boolean> {
-        return this.userRepository
-          .findOne({ where: { username } })
-          .then((user) => {
-            if (!user) {
-              return false; 
-            }
-            return bcrypt.compare(password, user.password).then((isMatch) => {
-              return isMatch; 
-            });
-          })
-          .catch(() => {
-            return false; 
-          });
-      }
-      
+  constructor(
+    @InjectRepository(user)
+    private readonly userRepository: Repository<user>,
+  ) {}
 
+  // Create User
+  createUser(createUserDto: CreateUserDto): Promise<user> {
+    return this.userRepository
+      .findOne({
+        where: [{ username: createUserDto.username }, { email: createUserDto.email }],
+      })
+      .then((existingUser) => {
+        if (existingUser) {
+          return Promise.reject(new Error('Username or Email already taken'));
+        }
+
+        return bcrypt.hash(createUserDto.password, 10).then((hashedPassword) => {
+          const user = this.userRepository.create({
+            username: createUserDto.username,
+            email: createUserDto.email,
+            password: hashedPassword,
+          });
+
+          return this.userRepository.save(user);
+        });
+      });
+  }
+
+  // Check if User Exists
+  userExists(username: string): Promise<boolean> {
+    return this.userRepository
+      .findOne({ where: { username } })
+      .then((user) => !!user);
+  }
+
+  // Fetch User Profile (Get User Details)
+  fetchUserProfile(id: number) {
+    return this.userRepository.findOneBy({ id });
+  }
+
+  // Validate User
+  validateUser(username: string, password: string): Promise<boolean> {
+    return this.userRepository
+      .findOne({ where: { username } })
+      .then((user) => {
+        if (!user) {
+          return false;
+        }
+        return bcrypt.compare(password, user.password).then((isMatch) => {
+          return isMatch;
+        });
+      })
+      .catch(() => {
+        return false;
+      });
+  }
+
+  // Find by Email
+  findByEmail(email: string): Promise<user | undefined> {
+    return this.userRepository.findOne({ where: { email } });
+  }
 }
